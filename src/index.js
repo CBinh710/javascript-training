d8f3cb0cb2efee7f1b27817f2b42ac9d3c6b516c
Cbinh710 <cbinh071002@gmail.com>
2024-09-18 16:05:21 +0700

    Update format and new ways to do exercise

--- src/index.js
+++ src/index.js
@@ -13,19 +13,20 @@ console.log(numberCount);
 //Using for loop
 let count = 0;
 
-for(let i = 0; randomValues.length > i; i++){
-  if(typeof randomValues[i] === 'number' ){
+for (let i = 0; randomValues.length > i; i++) {
+  if (typeof randomValues[i] === 'number') {
     count++
   }
 }
 console.log(count)
 
 // Exercise 2
-var randomNumbers = [10, 20, 50, 25, 70, 0, 55, 90];
+let randomNumbers = [10, 20, 50, 25, 70, 0, 55, 90];
 // 1. Find the item with value = 70 and 30 in randomNumbers. Find at least 4 ways to do this
 
 // Using indexOf()
 const index30 = randomNumbers.indexOf(30);
+
 const index70 = randomNumbers.indexOf(70);
 
 console.log(index30);
@@ -33,6 +34,7 @@ console.log(index70);
 
 // Using find()
 const item30 = randomNumbers.find(value => value === 30);
+
 const item70 = randomNumbers.find(value => value === 70);
 
 console.log(item30);
@@ -40,6 +42,7 @@ console.log(item70);
 
 // Using filter()
 const value30 = randomNumbers.filter(value => value === 30);
+
 const value70 = randomNumbers.filter(value => value === 70);
 
 console.log(value30);
@@ -47,6 +50,7 @@ console.log(value70);
 
 // Using for()
 let found30 = false;
+
 let found70 = false;
 
 for (let i = 0; randomNumbers.length > i; i++) {
@@ -62,6 +66,7 @@ console.log(found70);
 
 // 2. Find the biggest number in randomNumbers. Print result and index of them
 let maxNumber = randomNumbers[0];
+
 let maxIndex = 0;
 
 for (let i = 1; randomNumbers.length > i; i++) {
@@ -91,7 +96,7 @@ console.log(plusFiveLoop);
 // Using forEach()
 let newNumbers = [];
 
-randomNumbers.forEach(function(num){
+randomNumbers.forEach(function (num) {
   newNumbers.push(num + 5)
 });
-console.log
+console.log(newNumbers);
