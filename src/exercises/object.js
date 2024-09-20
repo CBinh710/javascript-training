7c008d580f5fb658713a6dd8de81f3f2d7031d96
Cbinh0710 <cbinh071002@gmail.com>
2024-09-20 14:36:56 +0700

    Update format and variable name in Object exercise

--- src/exercises/object.js
+++ src/exercises/object.js
@@ -1,35 +1,33 @@
 //Object exercise
 const users = [
-    { name: 'Kenny', age: 15 },
-    { name: 'Alice', age: 18 },
-    { name: 'Tom', age: 5 },
-    { name: 'Henry', age: 30 },
-    { name: 'Mike', age: 17 }, 
-  ];
-  // 1. Get all users that their ages are over 18
-  const usersOver18 = users.filter(user => user.age > 18);
+  { name: 'Kenny', age: 15 },
+  { name: 'Alice', age: 18 },
+  { name: 'Tom', age: 5 },
+  { name: 'Henry', age: 30 },
+  { name: 'Mike', age: 17 }, 
+];
+// 1. Get all users that their ages are over 18
+
+const MIN_AGE = 18; 
+const usersOver18 = users.filter(user => user.age > MIN_AGE);
   
-  console.log('Get all users that their ages are over 18');
-  console.log(usersOver18);
+console.log('Get all users that their ages are over 18: ', usersOver18);
   
-  // 2. Find user have letter “e” in their name
-  const usersWithE = users.filter(user => user.name.toLowerCase().includes('e'));
+// 2. Find user have letter “e” in their name
+const LETTER_TO_SEARCH = 'e';
+const usersWithE = users.filter(user => user.name.toLowerCase().includes(LETTER_TO_SEARCH));
   
-  console.log('Users have letter “e” in their name');
-  console.log(usersWithE);
+console.log('Users have letter “e” in their name: ', usersWithE);
   
-  // 3. Uppercase people name
-  const usersUpperCase = users.map(user => ({
-    name: user.name.toUpperCase(),
-    age: user.age
-  }));
+// 3. Uppercase people name
+const capitalizedUsers = users.map(user => ({
+  name: user.name.toUpperCase(),
+  age: user.age
+}));
   
-  console.log('Uppercase people name:');
-  console.log(usersUpperCase);
+console.log('Uppercase people name:', capitalizedUsers);
   
-  // 4. Order user by their ages from younger to older
-  const usersSortedByAge = users.sort((a, b) => a.age - b.age)
+// 4. Order user by their ages from younger to older
+const usersSortedByAge = users.sort((a, b) => a.age - b.age)
   
-  console.log('Order user by their ages from younger to older: ')
-  console.log(usersSortedByAge);
-  
+console.log('Order user by their ages from younger to older: ', usersSortedByAge)
