0caff88de647ba6ab9d1bcab64197c2a3d4ee21f
Cbinh0710 <cbinh071002@gmail.com>
2024-09-23 13:59:39 +0700

    Delete unnecessary comments

--- src/exercises/function.js
+++ src/exercises/function.js
@@ -14,28 +14,22 @@ users.forEach(users => {
 }
 
 printUsernames(users)
-  // 2. Write a function that find all users that their ages in ranges
-  // Ex: findUserInRange(users, fromAge, toAge)
+// 2. Write a function that find all users that their ages in ranges
 function findUserInRange(users, fromAge, toAge) {
   return users.filter(user => user.age >= fromAge && user.age <= toAge);
 }
 
-// Gọi hàm và in kết quả
 const usersInRange = findUserInRange(users, 10, 20);
 
 console.log(usersInRange);
-// Output:
-// [ { name: 'Kenny', age: 15 }, { name: 'Alice', age: 18 }, { name: 'Mike', age: 17 } ]
-
-  // 3. Write a function that accepts a function as input to handle users
-  // Ex: handleUserByAction(users, printName)
+// 3. Write a function that accepts a function as input to handle users
 function handleUserByAction(users, action) {
   users.forEach(user => {
     action(user);
   });
 }
 
-// Example: Pass a function to print user names and ages
+//Pass a function to print user names and ages
 handleUserByAction(users, function(user) {
   console.log(user.name + ' is ' + user.age + ' years old');
 });
