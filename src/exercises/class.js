952e352566cef6e745dfa9ba7f1ad64e46723d0a
Cbinh0710 <cbinh071002@gmail.com>
2024-09-24 09:19:18 +0700

    Ádding exercise content

--- src/exercises/class.js
+++ src/exercises/class.js
@@ -1,3 +1,21 @@
+// EXERCISE:
+// ---------------------
+// class User { }
+
+// Update class so we can use following code without error
+// var user = new User({name: “Ken”, age: 18})
+// user.name // Ken
+
+// Update class so we can use following code without error
+// var user = new User({name: “Ken”, age: 18})
+// user.getInfo() // This is Ken. He is 18 years old
+
+// Update class so we can use following code without error
+// var user = new User({name: “Ken”, age: 18})
+// var user2 = new User({name: “Mary”, age: 15})
+// user.isAdult() // true
+// user2.isAdult() // false
+
 class User {
   constructor({ name, age }) {
     this.name = name;
