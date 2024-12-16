54b4e12e309350346ffac57521ff0ecfd5e97674
Cbinh0710 <cbinh071002@gmail.com>
2024-12-16 10:50:41 +0700

    Create static data for milestone 2

---
+++ practice-1/src/scripts/index.js
@@ -0,0 +1,94 @@
+const staticData = {
+	products: [
+		{
+			id: 1,
+			imageURL: './assets/images/products/gradient-graphic-t-shirt.png',
+			name: 'Gradient Graphic T-Shirt',
+			price: 290,
+			discount: 10,
+			color: ['pink', 'blue', 'skyblue'],
+			size: ['Small', 'Medium', 'Large', 'X-Large'],
+			description: 'A must-have for days on the go, this adidas juniors hoodie is made from soft fleece to keep you comfortable wherever you go. Ribbed cuffs add comfort and a zip down the front lets you adjust the amount of coverage.'	
+		},
+		{
+			id: 2,
+			imageURL: './assets/images/products/gradient-graphic-t-shirt.png',
+			name: 'Gradient Graphic Shirt',
+			price: 190,
+			discount: 15,
+			color: ['pink', 'blue', 'skyblue'],
+			size: ['XX-Small', 'Medium', 'Large'],
+			description: 'A must-have for days on the go, this adidas juniors hoodie is made from soft fleece to keep you comfortable wherever you go. Ribbed cuffs add comfort and a zip down the front lets you adjust the amount of coverage.'	
+		},
+		{
+			id: 3,
+			imageURL: './assets/images/products/gradient-graphic-t-shirt.png',
+			name: 'Gradient Graphic T-Shirt',
+			price: 3900,
+			discount: 30,
+			color: ['orange', 'black', 'green'],
+			size: ['XX-Small', 'Large', 'X-Large'],
+			description: 'A must-have for days on the go, this adidas juniors hoodie is made from soft fleece to keep you comfortable wherever you go. Ribbed cuffs add comfort and a zip down the front lets you adjust the amount of coverage.'	
+		},
+		{
+			id: 4,
+			imageURL: './assets/images/products/gradient-graphic-t-shirt.png',
+			name: 'Gradient Graphic T-Shirt',
+			price: 1900,
+			discount: 20,
+			color: ['gold', 'blue', 'green'],
+			size: ['X-Small', 'Medium', 'XXX-Large'],
+			description: 'A must-have for days on the go, this adidas juniors hoodie is made from soft fleece to keep you comfortable wherever you go. Ribbed cuffs add comfort and a zip down the front lets you adjust the amount of coverage.'	
+		},
+		{
+			id: 5,
+			imageURL: './assets/images/products/gradient-graphic-t-shirt.png',
+			name: 'Gradient Graphic T-Shirt',
+			price: 650,
+			discount: 50,
+			color: ['gold', 'pink', 'green'],
+			size: ['X-Small', 'Medium', 'Large', 'X-Large'],
+			description: 'A must-have for days on the go, this adidas juniors hoodie is made from soft fleece to keep you comfortable wherever you go. Ribbed cuffs add comfort and a zip down the front lets you adjust the amount of coverage.'	
+		},
+		{
+			id: 6,
+			imageURL: './assets/images/products/gradient-graphic-t-shirt.png',
+			name: 'Gradient Graphic T-Shirt',
+			price: 1900,
+			discount: 30,
+			color: ['gold', 'black', 'skyblue'],
+			size: ['XX-Small', 'Large', 'X-Large'],
+			description: 'A must-have for days on the go, this adidas juniors hoodie is made from soft fleece to keep you comfortable wherever you go. Ribbed cuffs add comfort and a zip down the front lets you adjust the amount of coverage.'	
+		},
+		{
+			id: 7,
+			imageURL: './assets/images/products/gradient-graphic-t-shirt.png',
+			name: 'Gradient Graphic T-Shirt',
+			price: 50,
+			discount: 40,
+			color: ['gold', 'black', 'skyblue'],
+			size: ['Medium', 'Large', 'X-Large'],
+			description: 'A must-have for days on the go, this adidas juniors hoodie is made from soft fleece to keep you comfortable wherever you go. Ribbed cuffs add comfort and a zip down the front lets you adjust the amount of coverage.'	
+		},
+		{
+			id: 8,
+			imageURL: './assets/images/products/gradient-graphic-t-shirt.png',
+			name: 'Gradient Graphic T-Shirt',
+			price: 2000,
+			discount: 10,
+			color: ['violet', 'blue', 'skyblue'],
+			size: ['Small', 'Medium', 'X-Large', 'XX-Large'],
+			description: 'A must-have for days on the go, this adidas juniors hoodie is made from soft fleece to keep you comfortable wherever you go. Ribbed cuffs add comfort and a zip down the front lets you adjust the amount of coverage.'	
+		},
+		{
+			id: 9,
+			imageURL: './assets/images/products/gradient-graphic-t-shirt.png',
+			name: 'Gradient Graphic T-Shirt',
+			price: 1900,
+			discount: 24,
+			color: ['violet', 'blue', 'skyblue'],
+			size: ['Small', 'Medium', 'Large', 'X-Large'],
+			description: 'A must-have for days on the go, this adidas juniors hoodie is made from soft fleece to keep you comfortable wherever you go. Ribbed cuffs add comfort and a zip down the front lets you adjust the amount of coverage.'	
+		}
+	]
+};
